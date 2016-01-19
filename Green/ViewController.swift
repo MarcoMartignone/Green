ed6a7d7e630bb98741de136a103ddf222b716dd0
Marco Martignone <marco.martignone@me.com>
2016-01-19 16:26:31 +0000

    Starting recording implementation

--- Green/ViewController.swift
+++ Green/ViewController.swift
@@ -3,6 +3,7 @@ import AudioToolbox
 import GPUImage
 import AssetsLibrary
 import FBSDKMessengerShareKit
+import QuartzCore
 
 class ViewController: UIViewController, UIGestureRecognizerDelegate{
     var videoCamera:GPUImageVideoCamera?
@@ -10,25 +11,23 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate{
     var pathToMovie: String!
     var effectSlider: float_t!
     var savedImage: UIImage!
-    
-    @IBOutlet weak var gpuImage: GPUImageView!
     var backgroundMovie: GPUImageMovie!
-
+    var movieWriter: GPUImageMovieWriter!
+    @IBOutlet weak var gpuImage: GPUImageView!
+    @IBOutlet var borderView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "resumeActivities", name: UIApplicationDidBecomeActiveNotification, object: nil);
-        
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "stopActivities", name: UIApplicationWillResignActiveNotification, object: nil);
         
         effectSlider = 0.4
-        pathToMovie = ""
         
-        self.view.backgroundColor = UIColor.whiteColor()
-        gpuImage.backgroundColor = UIColor.whiteColor()
+        self.view.backgroundColor = UIColor.redColor()
+        gpuImage.backgroundColor = UIColor.clearColor()
 
-        if let resourceUrl = NSBundle.mainBundle().URLForResource("do", withExtension: "mp4") {
+        if let resourceUrl = NSBundle.mainBundle().URLForResource("galaxy", withExtension: "mp4") {
             print(resourceUrl)
             if NSFileManager.defaultManager().fileExistsAtPath(resourceUrl.path!) {
                 backgroundMovie = GPUImageMovie.init(URL: resourceUrl)
@@ -50,24 +49,21 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate{
         swipeUp.direction = UISwipeGestureRecognizerDirection.Up
         gpuImage.addGestureRecognizer(swipeUp)
         
+        let longPress = UILongPressGestureRecognizer(target: self, action: "longPressAction:")
+        gpuImage.addGestureRecognizer(longPress)
+        
+        // Setup borderView
+        borderView.backgroundColor = UIColor.clearColor()
+        borderView.layer.borderColor = UIColor.whiteColor().CGColor
+        borderView.layer.borderWidth = 8
+        borderView.alpha = 0;
+
         // Setup camera processing
         setupCamera()
     }
     
-    // Resume Processing
-    func resumeActivities() {
-        videoCamera?.startCameraCapture()
-        backgroundMovie.startProcessing()
-    }
-    
-    // Suspend processing
-    func stopActivities() {
-        videoCamera!.stopCameraCapture()
-        backgroundMovie.endProcessing()
-    }
-    
     // Called on start
-    func setupCamera(){
+    func setupCamera() {
         videoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSessionPresetHigh, cameraPosition: .Back)
         
         videoCamera!.outputImageOrientation = .Portrait;
@@ -87,10 +83,32 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate{
         filter?.addTarget(gpuImage)
     }
     
+    func longPressAction(gestureRecognizer:UIGestureRecognizer) {
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            setupRecording()
+        } else if gestureRecognizer.state == UIGestureRecognizerState.Ended {
+            movieWriter.finishRecording()
+        }
+    }
+    
+    func setupRecording() {
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+        pathToMovie = documentsPath + "/stream.m4v"
+        print("pathToMovie \(pathToMovie)")
+        unlink((pathToMovie as NSString).UTF8String)
+        let movieURL = NSURL.fileURLWithPath(pathToMovie)
+        movieWriter = GPUImageMovieWriter(movieURL: movieURL, size: CGSizeMake(680, 480))
+        movieWriter!.encodingLiveVideo = true
+        
+        movieWriter.startRecording()
+    }
+    
     func captureThis() {
         let image = (filter?.imageFromCurrentFramebuffer())!
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        
         AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+        flashBorderView()
         
         savedImage = image
         
@@ -109,6 +127,35 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate{
     func sharePhoto() {
         if (savedImage != nil) {
             FBSDKMessengerSharer.shareImage(savedImage, withOptions: nil)
+        }
+    }
+    
+    // Resume Processing
+    func resumeActivities() {
+        videoCamera?.startCameraCapture()
+        backgroundMovie.startProcessing()
+    }
+    
+    // Suspend processing
+    func stopActivities() {
+        videoCamera!.stopCameraCapture()
+        backgroundMovie.endProcessing()
+        movieWriter.finishRecording()
+        
+        videoCamera!.stopCameraCapture()
+        movieWriter!.finishRecordingWithCompletionHandler { () -> Void in
+            //            if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(self.pathToMovie))
+            
+            UISaveVideoAtPathToSavedPhotosAlbum(self.pathToMovie, self,
+                "video:didFinishSavingWithError:contextInfo:", nil)
+        }
+    }
+    
+    func flashBorderView() {
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            self.borderView.alpha = 1
+            }) { (Bool) -> Void in
+                self.borderView.alpha = 0;
         }
     }
     
